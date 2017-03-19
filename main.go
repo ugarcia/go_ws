8f605326a01b6ad452e19b48675401e3a2f98dc0
unai <unai.gg77@gmail.com>
2017-03-19 20:26:06 +0100

    Basic config

---
+++ main.go
@@ -0,0 +1,21 @@
+/*
+Go Gigs - Main Package.
+
+Initializes database connection and http server.
+ */
+package main
+
+import (
+    // "gitlab.com/fc_freelance/go_gigs/db"
+    // "gitlab.com/fc_freelance/go_gigs/http"
+)
+
+// Main execution thread
+func main() {
+
+    // Init database connection
+    // db.Init()
+
+    // Init HTTP server
+    // http.Init()
+}
